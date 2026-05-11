e732a05967521c1dd2f1cb022c286c5c25279a17
Brayam Programador <brateomx@gmail.com>
2026-05-11 15:57:19 -0600

    quitar elementos dark

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -3,8 +3,8 @@
     <head>
         @include('partials.head')
     </head>
-    <body class="min-h-screen bg-white ">
-        <flux:sidebar sticky stashable class="border-r border-zinc-200 bg-zinc-50 ">
+    <body class="min-h-screen bg-slate-50">
+        <flux:sidebar sticky stashable class="border-r border-slate-300 bg-white">
             <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
             <a href="{{ route('dashboard') }}" class="mr-5 flex items-center space-x-2" wire:navigate>
@@ -29,7 +29,6 @@
                 </flux:navlist.item>
             </flux:navlist>
 
-            <!-- Desktop User Menu -->
             <flux:dropdown position="bottom" align="start">
                 <flux:profile
                     :name="auth()->user()->name"
@@ -43,15 +42,15 @@
                             <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                                 <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
                                     <span
-                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black "
+                                        class="flex h-full w-full items-center justify-center rounded-lg bg-slate-200 text-slate-800 font-semibold"
                                     >
                                         {{ auth()->user()->initials() }}
                                     </span>
                                 </span>
 
                                 <div class="grid flex-1 text-left text-sm leading-tight">
-                                    <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
-                                    <span class="truncate text-xs">{{ auth()->user()->email }}</span>
+                                    <span class="truncate font-semibold text-slate-800">{{ auth()->user()->name }}</span>
+                                    <span class="truncate text-xs text-slate-600">{{ auth()->user()->email }}</span>
                                 </div>
                             </div>
                         </div>
@@ -75,7 +74,6 @@
             </flux:dropdown>
         </flux:sidebar>
 
-        <!-- Mobile User Menu -->
         <flux:header class="lg:hidden">
             <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
 
@@ -93,15 +91,15 @@
                             <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                                 <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
                                     <span
-                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black "
+                                        class="flex h-full w-full items-center justify-center rounded-lg bg-slate-200 text-slate-800 font-semibold"
                                     >
                                         {{ auth()->user()->initials() }}
                                     </span>
                                 </span>
 
                                 <div class="grid flex-1 text-left text-sm leading-tight">
-                                    <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
-                                    <span class="truncate text-xs">{{ auth()->user()->email }}</span>
+                                    <span class="truncate font-semibold text-slate-800">{{ auth()->user()->name }}</span>
+                                    <span class="truncate text-xs text-slate-600">{{ auth()->user()->email }}</span>
                                 </div>
                             </div>
                         </div>
