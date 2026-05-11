c29e703929ade5263a9c19e6e40e7e5cf74c5f30
Brayam Programador <brateomx@gmail.com>
2026-05-11 16:06:38 -0600

    cambio de nombre para los proyectos

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -2,5 +2,5 @@
     <x-app-logo-icon class="size-5 fill-current text-white " />
 </div>
 <div class="ml-1 grid flex-1 text-left text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+    <span class="mb-0.5 truncate leading-none font-semibold">Laboratorio Intimak</span>
 </div>
