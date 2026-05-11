412bc2a99cb172a5dcf21102a45e5e04e8d1db26
Brayam Programador <brateomx@gmail.com>
2026-05-11 17:07:17 -0600

    quitar opcion de recuperacion de contraseña

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -94,11 +94,6 @@ new #[Layout('components.layouts.auth')] class extends Component {
                 placeholder="Contraseña"
             />
 
-            @if (Route::has('password.request'))
-                <x-text-link class="absolute right-0 top-0" href="{{ route('password.request') }}">
-                    ¿Olvidaste tu contraseña?
-                </x-text-link>
-            @endif
         </div>
 
         <!-- Remember Me -->
